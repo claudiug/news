465135d0661b405e0ad43ce41c774044730b1f01
claudiu.garba <claudiu.garba@gmail.com>
2013-04-01 00:32:55 +0200

    add

--- demo.py
+++ demo.py
@@ -1,2 +1,20 @@
-__author__ = 'claudiu.garba'
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+site = urlopen("https://news.ycombinator.com/")
+soup = BeautifulSoup(site.read())
+print soup.title
+link = soup.find_all('a')
+nycontent = []
+for l in link:
+    if not l.string == None:
+        nycontent.append(l.contents)
 
+def add_uniq(seq):
+    c = []
+    for x in seq:
+        if x not in c:
+            c.append(x)
+    return c
+
+print len(add_u(nycontent))
+print len(nycontent)
